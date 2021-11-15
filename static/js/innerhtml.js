11684da09e033520714ff668dc52181423b776f6
tokap0 <takachin5353@gmail.com>
2021-11-15 10:18:58 +0900

    first commit

---
+++ static/js/innerhtml.js
@@ -0,0 +1,6 @@
+
+function button_action(){
+        sink_element = document.getElementById("sink_element");
+        document.getElementById("dom_sink_element").innerHTML = sink_element.value;
+}
+
