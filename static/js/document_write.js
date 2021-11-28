efbd5dc7bfe41eaf4664411a966408de625b01a7
tokap0 <takachin5353@gmail.com>
2021-11-29 01:11:04 +0900

    change workflow and js file

--- static/js/document_write.js
+++ static/js/document_write.js
@@ -1,5 +1,5 @@
 function button_action(){
     var element = document.getElementById("sink_element");
     console.log(element);
-    document.write(mypolicy.createHTML(element.value));
+    document.write(element.value);
 }
